a5a30602c99e522d81880dd4bb17bca665535072
endayshebocah <endayshebocah@gmail.com>
2025-09-15 01:40:26 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Tambahkan baris ini jika Anda berencana deploy ke GitHub Pages
-  // Gantilah 'NAMA_REPOSITORY_ANDA' dengan nama repository GitHub Anda
-  // base: '/NAMA_REPOSITORY_ANDA/', 
+  base: "./", // <--- penting untuk deploy di Netlify
   build: {
     outDir: 'dist',
   },
   server: {
-    port: 3000, // Anda bisa mengganti port jika diperlukan
+    port: 3000,
   },
 });
