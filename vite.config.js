2a9c2d437d16f8c19d3266ea63bbbe51dfa93282
endayshebocah <endayshebocah@gmail.com>
2025-09-15 01:45:24 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // <--- penting untuk deploy di Netlify
+  base: "./", // penting untuk memastikan asset JS/CSS bisa di-load di Netlify
   build: {
     outDir: 'dist',
   },
