3c712df4089b556ecceaab13751b4ecba9c698ae
endayshebocah <endayshebocah@gmail.com>
2025-09-20 14:47:11 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,42 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo192.png', 'logo512.png'],
+      manifest: {
+        name: "Database Manajemen Peserta Kokuo",
+        short_name: "Kokuo Tc",
+        description: "Aplikasi manajemen peserta",
+        theme_color: "#111827",
+        background_color: "#111827",
+        display: "standalone",
+        start_url: ".",
+        icons: [
+          {
+            src: "/logo192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
+  base: "./",
   build: {
-    // Menetapkan target build ke 'esnext' memungkinkan penggunaan
-    // fitur JavaScript modern seperti import.meta.env tanpa masalah.
-    target: 'esnext'
+    outDir: "dist"
+  },
+  server: {
+    port: 3000
   }
-});
+})
